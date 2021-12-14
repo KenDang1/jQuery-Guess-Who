bfaede563477dd38682036b1656693fbc042490c
Ken Dang <k3ndang@gmail.com>
2021-12-13 22:05:36 -0600

    add jquery.js and console.log to see if it work

--- client.js
+++ client.js
@@ -1,1 +1,18 @@
 console.log('Here are all the available people:', people);
+
+$(document).ready(onReady);
+
+function onReady() {
+    console.log('on ready');
+    
+
+
+};
+let picture = [];
+for (let person of people) {
+    if (person.name !== '')
+    picture += person.name;
+    $('body').append(`
+        <div class="avatar">${picture}</div>
+    `);
+};
